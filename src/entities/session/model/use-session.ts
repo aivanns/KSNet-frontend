6c67db4b0e59b7cc429b7a85ba409d2fa31b0470
aivanns <a1vazovski@yandex.ru>
2025-01-22 20:49:03 +0300

    added useSession hook

---
+++ src/entities/session/model/use-session.ts
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+import { useUserStore } from '@/entities/user/model/store'
+import authApi from '@/features/auth/api/auth'
+import { routes } from '@/shared/constants/routes'
+
+export const useSession = () => {
+  const router = useRouter()
+  const { user, fetchUser } = useUserStore()
+
+  const logout = async () => {
+    try {
+      await authApi.logout()
+      localStorage.removeItem('token')
+      router.push(routes.login)
+      toast.success('Вы успешно вышли из аккаунта')
+    } catch (error) {
+      toast.error('Ошибка при выходе из аккаунта')
+    }
+  }
+
+  const refreshSession = async () => {
+    try {
+      const { accessToken } = await authApi.refresh()
+      localStorage.setItem('token', accessToken)
+      await fetchUser()
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
+  return {
+    user,
+    isAuthenticated: !!user,
+    logout,
+    refreshSession
+  }
+} 
