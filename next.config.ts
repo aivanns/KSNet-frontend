5121f25219b3ae2e79c083470de7066fa426499e
aivanns <150065122+aivanns@users.noreply.github.com>
2025-02-04 14:54:51 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,7 @@ import fs from "fs";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  server: {
-    https: {
-      key: fs.readFileSync(path.join(process.cwd(), "certs", "private.key")),
-      cert: fs.readFileSync(
-        path.join(process.cwd(), "certs", "certificate.crt")
-      ),
-    },
-  },
+
 };
 
 export default nextConfig;
